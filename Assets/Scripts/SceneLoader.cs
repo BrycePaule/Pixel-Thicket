95af2e39eac4b99a7abbcb925d9778a5be792392
BrycePaule <egymlol@gmail.com>
2020-07-27 11:54:17 +1000

    - Added sceneloading functionality + crossfading

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField] private Animator _transition;
+    [SerializeField] private float _transitionTime = 1;
+
+
+
+    private void LoadGame()
+    {
+        StartCoroutine(LoadScene(1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        // player animation
+        _transition.SetTrigger("Start");
+
+        // wait
+        yield return new WaitForSeconds(_transitionTime);
+
+        // load scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
