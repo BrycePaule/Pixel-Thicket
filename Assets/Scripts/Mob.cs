95b70e25be5943960b8656b828c25a93ff5ebc4f
BrycePaule <egymlol@gmail.com>
2020-07-30 17:19:09 +1000

    - Added Mob movement mechanisms - Mobs will now have interchangable movestyles / movepattens that on the fly change how they move

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -6,56 +6,153 @@ public class Mob : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private CircleCollider2D _aggroRadius;
+    [SerializeField] private Animator _animator;
+    [Space(10)]
+    [SerializeField] private float _moveSpeedAggro;
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private MovementStyle _moveStyle;
+    [SerializeField] private MovementPattern _movePattern;
+    [SerializeField] [Range(0, 3)] private float _hopCooldown;
+    [Space(10)]
 
     // TESTING
     [SerializeField] private Player _player;
 
     private Transform _transform;
+    private bool _aggro = false;
+    private bool _hopWait;
 
     private Vector2 _dest;
-    private bool _destArrived = false;
+    private Vector2 _direction;
+    private bool _destArrived = true;
 
-    private void Awake() {
+    // UNITY METHODS
+    private void Awake() 
+    {
         _transform = transform;
+    }
+
+    public virtual void Start() 
+    {
+        // _direction = Vector2.zero;
     }
 
     private void Update()
     {
         Move();
-        // print(_dest);
     }
 
-    private void Move() {
+    // MOVEMENT
+    public virtual void Move() {
 
-        Vector2 direction = Vector2.zero;
-        float timeDelta = Time.deltaTime;
-
-        if (_destArrived | _dest == null | direction == Vector2.zero) 
-        { 
-            SelectNewDestination(); 
+        if (_movePattern == MovementPattern.FollowPlayer)
+        {
+            SetDestination();
+            SetDirection();
         }
 
-        _destArrived = false;
+        else if (_movePattern == MovementPattern.RandomLocation)
+        {
+            CheckArrived();
 
-        direction = _dest - _rb.position;
-        direction.Normalize();
+            if (_destArrived | _dest == null | _direction == Vector2.zero) 
+            { 
+                SetDestination(); 
+            }
 
-        _rb.position += new Vector2(direction.x * _moveSpeed * timeDelta, direction.y * _moveSpeed * timeDelta);
+            SetDirection();
+            _destArrived = false;
+        }
+
+        if (_moveStyle == MovementStyle.Standard) 
+        {
+            _animator.SetBool("Hopping", false);
+
+            if (_aggro)
+            {
+                // _animator.speed =
+                _rb.position += new Vector2(_direction.x * _moveSpeedAggro * Time.deltaTime, _direction.y * _moveSpeedAggro * Time.deltaTime);
+            }
+            else
+            {
+                _rb.position += new Vector2(_direction.x * _moveSpeed * Time.deltaTime, _direction.y * _moveSpeed * Time.deltaTime);
+            }
+
+        }
+
+        else if (_moveStyle == MovementStyle.Hop)
+        {
+            _animator.SetBool("Hopping", true);
+
+            if (!_hopWait)
+            {
+                if (_aggro)
+                {
+                    _rb.position += new Vector2(_direction.x * _moveSpeedAggro * Time.deltaTime, _direction.y * _moveSpeedAggro * Time.deltaTime);
+                }
+                else
+                {
+                    _rb.position += new Vector2(_direction.x * _moveSpeed * Time.deltaTime, _direction.y * _moveSpeed * Time.deltaTime);
+                }
+            }
+        } 
     }
 
-    private void SelectNewDestination()
+    private void SetDestination()
     {
-        // print("new dest");
-        _dest = new Vector2((int) Random.Range(-5, 5), (int) Random.Range(-5, 5));
-        // _dest = _player.GetComponent<Rigidbody2D>().position;
-        // print(_dest);
+        if (_movePattern == MovementPattern.FollowPlayer)
+        {
+            _dest = _player.GetComponent<Rigidbody2D>().position;
+        }
+
+        else if (_movePattern == MovementPattern.RandomLocation)
+        {
+            _dest = new Vector2((int) Random.Range(-10, 10), (int) Random.Range(-10, 10));
+        }
+        
     }
 
+    private void SetDirection()
+    {
+        _direction = _dest - _rb.position;
+        _direction.Normalize();
+
+        // Update direction in animator
+        _animator.SetFloat("Horizontal", _direction.x);
+        _animator.SetFloat("Vertical", _direction.y);
+    }
+
+    private void CheckArrived()
+    {
+        if (Mathf.RoundToInt(_rb.position.x) != _dest.x) { return; }
+        if (Mathf.RoundToInt(_rb.position.y) != _dest.y) { return; }
+
+        _destArrived = true;
+    }
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
         _destArrived = true;
     }
 
+    private IEnumerator HopWait()
+    {
+        _hopWait = true;
+        yield return new WaitForSeconds(_hopCooldown);
+        _hopWait = false;
+    }
+    
+    // UTILITIES
+    private enum MovementStyle
+    {
+        Standard,
+        Hop
+    }
+
+    private enum MovementPattern
+    {
+        RandomLocation,
+        FollowPlayer,
+    }
 }
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -5,6 +5,25 @@ using UnityEngine;
 public class Slime : Mob
 {
 
+    [SerializeField] private SpriteRenderer _sr;
+    [SerializeField] private Color _color;
+
     private int _generation;
+
+
+    // private void Awake()
+    // {
+
+    // }
+
+    public override void Start()
+    {
+        _sr.color = _color;
+    }
+
+    // public override void Move()
+    // {
+
+    // }
 
 }
